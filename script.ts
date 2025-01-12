faad2b2a9fe4bf34ad592f7e19823efd4b6d2c0e
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-01-12 23:37:05 +0530

    Added definition

--- script.ts
+++ script.ts
@@ -1,4 +1,6 @@
+// typescript is a superset of javascript. It is a statically typed language. It is used to develop large applications. It is a strongly typed language. It is a compiled
+
 var a = 14;
 var b = 10;
-// var a="shubham"; error because a is already declared with type number.   
+// var a="shubham"; error because a is already declared with type number.
 var a = 20;
